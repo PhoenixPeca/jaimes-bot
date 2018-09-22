9d383f5e3b94471b88a2138656022171b70e144b
Phoenix Eve Aspacio <aspaciop@phoenixpeca.xyz>
2018-09-21 20:52:21 -0400

    add index file

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+die('<html>
+<head><title>Hello Stranger!</title></head>
+<body><h1>Hello Stranger!</h1>
+<p>Welcome to The Amazing Endpoint.</p><hr>
+<address>Jaimes.AI.Bot</address></body>
+</html>');
