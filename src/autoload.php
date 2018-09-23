42302a105c2a1cad4422f7c370c579d5659dbe62
Phoenix Eve Aspacio <aspaciop@phoenixpeca.xyz>
2018-09-22 21:59:43 -0400

    use standard autoloading

--- src/autoload.php
+++ src/autoload.php
@@ -1,6 +1,9 @@
 <?php
 
-foreach (glob("src/*/*.php") as $class) {
-    if (file_exists($class))
-        include $class;
-}
+spl_autoload_register(function ($className) {
+    $ds = DIRECTORY_SEPARATOR;
+    $dir = __DIR__;
+    $className = str_replace('\\', $ds, $className);
+    $file = "{$dir}{$ds}{$className}.php";
+    if (is_readable($file)) require_once $file;
+});
