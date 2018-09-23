0515e256083459a7d2105bd2c7f68bbda2578941
Phoenix Eve Aspacio <aspaciop@phoenixpeca.xyz>
2018-09-22 22:57:09 -0400

    obfuscate

--- src/PreCheckAuthenticity/FacebookIntegrityCheck.php
+++ src/PreCheckAuthenticity/FacebookIntegrityCheck.php
@@ -18,13 +18,17 @@ class FacebookIntegrityCheck
            $AllHeaders{'Content-Type'} == 'application/json' &&
            $AllHeaders{'X-Hub-Signature'} == self::payloadSignature('sha1')) &&
            !HubVerify::getChallenge()) {
-            #header("HTTP/1.1 401 Unauthorized");
-            #die('You are not allowed to access this communication endpoint.');
-            die('<html><head><title>Welcome Stranger!</title></head><body><h1>Welcome</h1>'.
-                '<p>This is The Endpoint &mdash; a portal to the beautiful mind of compute'.
-                'r artificial intelligence.</p><hr><address>Jaimes Bot (<a href="https://g'.
-                'ithub.com/PhoenixPeca/jaimes-bot">jaimes.ai.bot</a>) for Facebook Messeng'.
-                'er</address></body></html>');
+            die(base64_decode('PCFET0NUWVBFIEhUTUwgUFVCTElDICItLy9JRVRGLy9EVEQ'.
+                              'gSFRNTCAyLjAvL0VOIj48aHRtbD48aGVhZD48dGl0bGU+V2'.
+                              'VsY29tZSBTdHJhbmdlciE8L3RpdGxlPjwvaGVhZD48Ym9ke'.
+                              'T48aDE+V2VsY29tZTwvaDE+PHA+VGhpcyBpcyBUaGUgRW5k'.
+                              'cG9pbnQgJm1kYXNoOyBhIHBvcnRhbCB0byB0aGUgYmVhdXR'.
+                              'pZnVsIG1pbmQgb2YgY29tcHV0ZXIgYXJ0aWZpY2lhbCBpbn'.
+                              'RlbGxpZ2VuY2UuPGJyIC8+PC9wPjxocj48YWRkcmVzcz5KY'.
+                              'WltZXMgQm90ICg8YSBocmVmPSJodHRwczovL2dpdGh1Yi5j'.
+                              'b20vUGhvZW5peFBlY2EvamFpbWVzLWJvdCI+amFpbWVzLmF'.
+                              'pLmJvdDwvYT4pIGZvciBGYWNlYm9vayBNZXNzZW5nZXI8L2'.
+                              'FkZHJlc3M+PC9ib2R5PjwvaHRtbD4='));
         }
         return true;
     }
