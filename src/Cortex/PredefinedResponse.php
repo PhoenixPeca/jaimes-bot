eaab7728f53b84ae2d13536f136367471350c344
Phoenix Eve Aspacio <aspaciop@phoenixpeca.xyz>
2018-09-26 23:33:49 -0400

    just a minor improvement

--- src/Cortex/PredefinedResponse.php
+++ src/Cortex/PredefinedResponse.php
@@ -8,9 +8,10 @@ class PredefinedResponse
 {
 
     public static function initiator($message) {
-        return ['Hi ' . UserProfile::getSenderData('name') . '!' .
-            'I am sorry, I\'m currently being developed. Most of my features aren\'t that ready for usage yet.',
-               ':( :('] ;
+        return ['Hi ' . UserProfile::getSenderData('name') . '! ' .
+                'I am sorry, I\'m currently being developed. ' .
+                'Most of my features aren\'t that ready for usage yet.',
+                ':( :('] ;
     }
 
 }
