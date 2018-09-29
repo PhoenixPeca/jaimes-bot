5584e65f49c7cc701980c69db3c1ffc177a7784a
Phoenix Eve Aspacio <aspaciop@phoenixpeca.xyz>
2018-09-29 02:37:41 -0400

    logic erro fix

--- src/Cortex/LanguageProcessor.php
+++ src/Cortex/LanguageProcessor.php
@@ -9,15 +9,12 @@ use Supplier\GeneralStatics;
 class LanguageProcessor
 {
 
-    private $gate_pass = false;
-
     public function feedIn($message) {
         if ($SynonThis = DictQueryExtract::getSynWord($message)) {
             unset($return);
             if ($SynonThis === true) {
                 $return = 'I\'m sorry but I can\'t think of a synonym for "' .
                           DictQueryExtract::getSynWord($message, true) . '".';
-                $this->gate_pass = true;
             } else {
                 $return = 'Synonyms of "' . $SynonThis . '": ' .
                             implode('; ', Dictionary::synFetch($SynonThis));
@@ -28,7 +25,6 @@ class LanguageProcessor
                 $return = ['I\'m sorry but "' . DictQueryExtract::getDefWord($message, true) .
                            '" is not yet in my vocabulary.',
                            'But don\'t worry I\'ll research about that later. 😉'];
-                $this->gate_pass = true;
             } else {
                 $return = '"' . ucfirst($DefineThis) . '" means ' .
                            GeneralStatics::arrRandomix(
@@ -40,7 +36,6 @@ class LanguageProcessor
                 $return = ['I\'m sorry but I don\'t know how to use "' .
                            DictQueryExtract::getExWord($message, true) . '" in a sentence.',
                            'Huhuhuhu!! 😥😥'];
-                $this->gate_pass = true;
             } else {
                 $return = 'Here is an example: "' . GeneralStatics::arrRandomix(
                                         Dictionary::exFetch($ExampleThis)) . '"';
@@ -51,22 +46,20 @@ class LanguageProcessor
             if ($TimeThis === true) {
                 $return = ['I\'m sorry but I don\'t know where that place is.',
                            'I\'m very sorry about that. 😥'];
-                $this->gate_pass = true;
             } else {
                 $return = 'The time' . (isset($TimeThis{1}) ? ' in ' . $TimeThis{1} :
                                         '').' is ' . Time::getFinalTime($TimeThis,
                                                                'g:i A (T l jS \of F Y)');
             }
         }
-        if (!isset($return) || empty($return) || $this->gate_pass) {
-            if ($PredefResp = PredefinedResponse::initiator($message,
-                                            GeneralStatics::getConfig('bot_vars'))) {
-                $return = $PredefResp;
-            } else {
-                $return = ['I was not explicitly taught to answer that ' .
-                               'statement of yours. I am still learning.',
-                           '😥😥😥'];
-            }
+        if ($PredefResp = PredefinedResponse::initiator($message,
+                                        GeneralStatics::getConfig('bot_vars'))) {
+            $return = $PredefResp;
+        }
+        if (!isset($return) || empty($return)) {
+            $return = ['I was not explicitly taught to answer that ' .
+                           'statement of yours. I am still learning.',
+                       '😥😥😥'];
         }
         return $return;
     }
